82542c40eb1083a360684f7e7b568a510118d02d
che-git <tinoausberlin@gmx.de>
2016-11-07 13:02:10 +0100

    Gui

---
+++ TripleLayer/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProduktVerwaltungTrippleLayer
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public Product Product;
+        public Customer Customer;
+        public int iAmount;
+        public DateTime OrderDate;
+    }
+}
