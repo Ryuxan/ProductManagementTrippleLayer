161d7dd875b6b4728300c6831b1378500474d386
che-dev <tinoausberlin@gmx.de>
2016-11-09 11:41:48 +0100

    read values from GUI-input

--- TripleLayer/MainWindow.xaml.cs
+++ TripleLayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProduktVerwaltungTrippleLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,24 +21,41 @@ namespace TripleLayer
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private IFachkonzept fachKonzept;
+
+        public MainWindow(IFachkonzept fk)
         {
+            this.fachKonzept = fk;
             InitializeComponent();
         }
 
         private void btn_create_order(object sender, RoutedEventArgs e)
         {
-
+            int customerId = 0;
+            int productId = 0;
+            Customer c = fachKonzept.GetCustomer(customerId);
+            Product p = fachKonzept.GetProduct(productId);
+            int amount;
+            int.TryParse(tbx_amount.Text, out amount);
+            Order order = new Order(c, p, amount, DateTime.Now);        
+            fachKonzept.AddOrder(order);
         }
 
         private void btn_create_product(object sender, RoutedEventArgs e)
         {
-
+            string label = tbx_product_name.Text;
+            double price;
+            double.TryParse(tbx_product_price.Text, out price);
+            Product product = new Product(label, price);
+            fachKonzept.AddProduct(product);
         }
 
-        private void btn_create_costomer(object sender, RoutedEventArgs e)
+        private void btn_create_customer(object sender, RoutedEventArgs e)
         {
-
+            string firstName = tbx_customer_firstname.Text;
+            string surName = tbx_customer_surname.Text;
+            Customer customer = new Customer(firstName, surName);
+            fachKonzept.AddCostomer(customer);
         }
     }
 }
